ac9f3cec0bcbf1152b7dfa279a91c3679db2a19d
hazlomcodes <harry.lomas1@outlook.com>
2023-03-08 17:15:41 +0000

    list all to do working

--- client/src/components/ListTodo.js
+++ client/src/components/ListTodo.js
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 const ListTodos = () => {
-    return <h1> ListTodos</h1>
+
+
+    const [todos, setTodos] = useState([]);
+
+
+    const getTodos = async() => {
+
+        try {
+
+            const response = await fetch("http://localhost:5000/todos")
+            const jsonData = await response.json()
+            setTodos(jsonData);
+            
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+    
+    //ensures only makes 1 request
+
+    useEffect(() => {
+        getTodos();
+    }, []);
+
+    
+    
+    
+    return <Fragment>
+
+
+<table class="table mt-5 text-center">
+    <thead>
+      <tr>
+        <th>Description</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+       <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      {todos.map(todo => (
+        <tr>
+            <td>{todo.description}</td>
+            <td>Edit</td>
+            <td>Delete</td>
+        </tr>
+      ))
+      }
+      </tbody>
+  </table>
+
+        
+        </Fragment>
 }
 
 export default ListTodos;
